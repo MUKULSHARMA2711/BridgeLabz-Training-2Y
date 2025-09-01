6c2cd4dbebe132e038d7fb098af3202fcb45034a
MUKULSHARMA2711 <mukul.kumar_cs.aiml24@gla.ac.in>
2025-09-02 00:19:59 +0530

    [Mukul Sharma] Added

---
+++ Arrays/Level2/FriendsCheck.java
@@ -0,0 +1,38 @@
+package Level2;
+import java.util.Scanner;
+
+class FriendsCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+            System.out.print("Enter height of " + names[i] + ": ");
+            heights[i] = sc.nextDouble();
+        }
+
+        // find youngest
+        int youngestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+
+        // find tallest
+        int tallestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        System.out.println("Youngest: " + names[youngestIndex]);
+        System.out.println("Tallest: " + names[tallestIndex]);
+    }
+}
